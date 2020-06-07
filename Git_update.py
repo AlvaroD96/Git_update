f50eef4ec3d64d55d1243231e0674c27ac432217
AlvaroD96 <alvduque@alumnos.uai.cl>
2020-06-07 17:47:08 -0400

    Second commit

--- Git_update.py
+++ Git_update.py
@@ -5,26 +5,26 @@ import os
 
 carpeta = 'LoadFiles'
 # listamos los archivos que se cargaran
-entries = os.listdir(carpeta)
+entries = os.listdir()
 for archivo in entries: 
     file_list = []
     #Separamos solo los archivos excel del directorio
     if archivo.endswith('.xlsx'):
-        print(archivo) 
-        file_list.append(carpeta+'/'+archivo)
+        
+        
+        # Designamos el nombre del repositorio al cual se subiran los archivos
 
-# Designamos el nombre del repositorio al cual se subiran los archivos
-print(file_list)
-repo_dir = 'Git_update'
-repo = Repo(repo_dir)
-
-
-#Se define el commit que se utilizara
-commit_message = 'Datos de series y portafolio fecha' + str(dt.datetime.now())
-#Se realiza el add de los archivos, que listamos antes
-repo.index.add(carpeta)
-#Se realiza el commit
-repo.index.commit(commit_message)
-#Se define la branche 
-origin = repo.remote('origin')
-origin.push()
+        repo_dir = 'GIT_UPDATE'
+        repo = Repo(repo_dir)
+        file_list = [archivo]
+        print(file_list)
+        #Se define el commit que se utilizara
+        commit_message = 'Datos de series y portafolio fecha' + str(dt.datetime.now())
+        print(commit_message)
+        #Se realiza el add de los archivos, que listamos antes
+        repo.index.add(file_list)
+        #Se realiza el commit
+        repo.index.commit(commit_message)
+        #Se define la branche 
+        origin = repo.remote('origin')
+        origin.push()
